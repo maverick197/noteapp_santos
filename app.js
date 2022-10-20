78fe8b7ac4db1d7a08239ba52ed758490fce3d2d
Maverick <mavericksantos2001@gmail.com>
2022-10-20 21:39:00 +0800

    added add.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+const add = require('./add')
 const write = require('./write')
 const read = require('./read')
 const present = require('./present')
+
+const data = process.argv
+
+var note = {}
+
+if(data[2] == 'add') {
+   
+    note = {
+        id: data[3],
+        title: data[4],
+        body: data[5]
+    }
+   
+    var oldNote = read()
+   
+    add(note, oldNote);
+}
+if(data[2] == 'read') {
+    
+    const present = require('./present')
+
+    present(read())
+}
