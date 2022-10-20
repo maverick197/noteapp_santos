7fb38926fc6b6c01897ce236ed8a21a70ff4348a
Maverick <mavericksantos2001@gmail.com>
2022-10-20 21:41:35 +0800

    added del.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const add = require('./add')
 const write = require('./write')
 const read = require('./read')
 const present = require('./present')
+const del = require('./del')
 
 const data = process.argv
 
@@ -24,4 +25,11 @@ if(data[2] == 'read') {
     const present = require('./present')
 
     present(read())
+}
+if(data[2] === "delete") {
+    let id=data[3]
+    let oldNote=read()
+    let del=require('./del')
+    del(id,oldNote);
+    console.log(read());
 }
---
+++ del.js
@@ -0,0 +1,10 @@
+const fs =require('fs')
+
+const del = function(id, oldNote) {
+    let note = JSON.parse(oldNote)
+    let newNote = note.filter(function(n,idx){
+        return n.id !== id
+    })
+    fs.writeFileSync('./note.txt', JSON.stringify(newNote))
+}
+module.exports = del
